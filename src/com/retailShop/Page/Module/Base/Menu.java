2179b8c00b1a984c5f2e6a4a3e888ffb1906f738
Tudorache Leonard Valentin <valileo2000@gmail.com>
2021-02-28 16:15:46 +0200

    - Added database

--- src/com/retailShop/Page/Module/Base/Menu.java
+++ src/com/retailShop/Page/Module/Base/Menu.java
@@ -1,14 +1,35 @@
-package com.retailShop.Page.Module.Base;
+package com.retailShop.page.module.base;
 
-import com.retailShop.Page.ContentHandler;
+import com.retailShop.page.ContentHandler;
+import com.retailShop.page.module.base.components.ComponentsContent;
+import com.retailShop.page.module.base.employees.EmployeesContent;
+import com.retailShop.page.module.base.logs.LogsContent;
+import com.retailShop.page.module.base.purchases.PurchasesContent;
+import com.retailShop.page.Permission;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
 
+public class Menu extends JPanel {
+    private ContentHandler contentHandler;
 
-public abstract class Menu extends JPanel {
-    public abstract void buildUi();
+    public Menu(ContentHandler contentHandler) {
+        this.contentHandler = contentHandler;
+    }
+
+    public void buildUi() {
+        applyDefaultSettings(contentHandler);
+
+        if (Permission.userCan("access", "EmployeesPage"))
+            addButtonToMenu("Manage Employees", contentHandler, new EmployeesContent(contentHandler));
+        if (Permission.userCan("access", "ComponentsPage"))
+            addButtonToMenu("Manage Components",contentHandler, new ComponentsContent(contentHandler));
+        if (Permission.userCan("access","PurchasesPage"))
+            addButtonToMenu("Manage Purchases",contentHandler, new PurchasesContent(contentHandler));
+        if (Permission.userCan("access","LogsPage"))
+            addButtonToMenu("Manage Purchases",contentHandler, new LogsContent(contentHandler));
+    }
 
     public void applyDefaultSettings(ContentHandler contentHandler) {
         setLayout(new MigLayout("","[]",""));
@@ -17,6 +38,7 @@ public abstract class Menu extends JPanel {
     }
 
     public void addButtonToMenu(String buttonText, ContentHandler contentHandler, Content linkTo) {
+
         JButton button = new JButton(buttonText);
         add(button, "w 100%, wrap");
 
